44194f6b379219f38a2d13cc18e7dd94d8925933
AUS\GOYALV5M <goyal.vrushali@abc.net.au>
2022-04-02 01:39:44 +1100

    update4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
-let dotenv = require('dotenv');
+var dotenv = require('dotenv');
 var app = express();
 
 var http = require('http');
@@ -8,14 +8,7 @@ var server = http.Server(app);
 
 app.use(express.static('client'));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'First-SDK', 'build')));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'First-SDK', 'build', 'index.html'))
-    });
-    
-  }
+
 
 server.listen(PORT, function() {
   console.log('Chat server running');
